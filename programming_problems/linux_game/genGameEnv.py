397e9e9d9310488cd9755f696ada383f0e5fc7b9
Joey Bahary <jbahary2@illinois.edu>
2019-01-19 20:35:58 -0600

    Added caching json and implemented a part of checking func

--- programming_problems/linux_game/genGameEnv.py
+++ programming_problems/linux_game/genGameEnv.py
@@ -1,12 +1,11 @@
 import json as j
-import hashlib as hl
+import hashlib
 import os
 import shutil
 import queue
 import requests
 import random
 import json
-import hashlib
 import string
 import argparse as ap
 
@@ -23,25 +22,56 @@ import argparse as ap
     Question 10: What is the frequency of the word appearing the maximum number of times in files, as file names, and as directory names?
 '''
 
-def delete(delete_name):
+NUM_QUESTIONS = 10
+
+def get_cache_name(name):
+    return "." + name + "_cache.json"
+
+def get_question_number(hashed_val, max_question):
+    for i in range(1, max_question + 1):
+        hash = hashlib.sha256(str("q" + str(i)).encode("utf-8")).hexdigest()
+        if hash == hashed_val:
+            return "q" + i
+    return "q0"
+def delete(args):
+    delete_name = args.delete_name
     if os.geteuid() == "root":
         print("Don't run as root")
     try:
+        print("Deleting game")
         shutil.rmtree(delete_name)
-        print("Deleting old game")
+        os.remove("." + delete_name + "")
     except Exception as e:
         pass
 
-def check(check_name):
-    pass # TODO create check function
+def check(args):
+    check_name = args.check_name
+    # TODO create check function
+    print("Checking your linux game answers... Cntl-C at any time to quit")
+    file = open(get_cache_name(check_name), "r")
+    contents = file.read()
+    json_dict = j.loads(contents)
+    current_question = get_question_number(json_dict["cq"], NUM_QUESTIONS)
+    while current_question in json_dict:
+        print(json_dict[current_question])
+        correct = False
+        answer = raw_input("")
+        while not correct:
+            hashed_answer = hashlib.sha256(answer.encode("utf-8")).hexdigest()
+            correct = hashed_answer == json_dict["a" + current_question[1:]] # Get the number of the question. Strip 'q' and add 'a'
+            if not correct:
+                print("Keep trying!")
+        print("Yay you got it!")
+        if current_question == NUM_QUESTIONS:
+            current_question
 
-def create(game_dir, max_dir_children, max_file_children, max_files, max_dirs, max_words):
-    GAME_NAME = game_dir
-    MAX_DIR_CHILDREN = max_dir_children
-    MAX_FILE_CHILDREN = max_file_children
-    MAX_FILES = max_files
-    MAX_DIRS = max_dirs
-    MAX_WORDS = max_words
+def create(args):
+    GAME_NAME = args.create_name
+    MAX_DIR_CHILDREN = args.max_dir_children
+    MAX_FILE_CHILDREN = args.max_file_children
+    MAX_FILES = args.max_files
+    MAX_DIRS = args.max_dirs
+    MAX_WORDS = args.max_words
     WORD_SITE = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain" # Name of site to get words from
 
     if os.geteuid() == "root":
@@ -49,6 +79,7 @@ def create(game_dir, max_dir_children, max_file_children, max_files, max_dirs, m
 
     try:
         shutil.rmtree(GAME_NAME)
+        os.remove(get_cache_name(GAME_NAME))
         print("Deleting old game")
     except Exception as e:
         pass
@@ -61,6 +92,7 @@ def create(game_dir, max_dir_children, max_file_children, max_files, max_dirs, m
     file_number = 0
     depth = 1
     bfs_q = queue.Queue(maxsize=0)
+    print("Requesting data from server...")
     resp = requests.get(WORD_SITE) # Get words for use in game
     words_list = [x.decode("utf-8").lower() for x in resp.content.splitlines()] #convert words to lowercase
 
@@ -215,34 +247,35 @@ def create(game_dir, max_dir_children, max_file_children, max_files, max_dirs, m
 
     # Last step is to hash and store the answers
     # Create hidden dir
-    cache_name = "." + game_dir + "cache.json"
+    cache_name = get_cache_name(GAME_NAME)
     json_dict = {}
-    json_dict["cq"] = h1.sha256("q1").hexdigest()
+    json_dict["cq"] = hashlib.sha256("1".encode("utf-8")).hexdigest()
     json_dict["q1"] = "What is the word in the file named {}".format(str(question_one_file_name))
-    json_dict["a1"] = hl.sha256(str(question_one_word_in_file)).hexdigest()
+    json_dict["a1"] = hashlib.sha256(str(question_one_word_in_file).encode("utf-8")).hexdigest()
     json_dict["q2"] = "How many words does the file named {} have?".format(str(question_two_file_name))
-    json_dict["a2"] = hl.sha256(str(question_two_num_words)).hexdigest()
+    json_dict["a2"] = hashlib.sha256(str(question_two_num_words).encode("utf-8")).hexdigest()
     json_dict["q3"] = "How many files and directories are there all together including the initial dir?"
-    json_dict["a3"] = hl.sha256(str(question_three_num_files_and_dirs)).hexdigest()
+    json_dict["a3"] = hashlib.sha256(str(question_three_num_files_and_dirs).encode("utf-8")).hexdigest()
     json_dict["q4"] = "How many files start with the letter {}? (Files not dirs)".format(str(question_four_letter))
-    json_dict["a4"] = hl.sha256(str(question_four_num_files)).hexdigest()
+    json_dict["a4"] = hashlib.sha256(str(question_four_num_files).encode("utf-8")).hexdigest()
     json_dict["q5"] = "What is the word on the third to last line of the file named {}?".format(str(question_five_file_name))
-    json_dict["a5"] = hl.sha256(str(question_five_word)).hexdigest()
+    json_dict["a5"] = hashlib.sha256(str(question_five_word).encode("utf-8")).hexdigest()
     json_dict["q6"] = "What is the size of the largest file? (In kilobytes)"
-    json_dict["a6"] = hl.sha256(str(question_six_largest_file_size)).hexdigest()
+    json_dict["a6"] = hashlib.sha256(str(question_six_largest_file_size).encode("utf-8")).hexdigest()
     json_dict["q7"] ="How many directories have {} many children?".format(str(question_seven_required_num_children))
-    json_dict["a7"] = hl.sha256(str(question_seven_num_directories)).hexdigest()
+    json_dict["a7"] = hashlib.sha256(str(question_seven_num_directories).encode("utf-8")).hexdigest()
     json_dict["q8"] = "What is the frequency of the word {} in the files of the directory tree?".format(str(question_eight_word_frequency))
-    json_dict["a8"] = hl.sha256(str(question_eight_word_frequency)).hexdigest()
+    json_dict["a8"] = hashlib.sha256(str(question_eight_word_frequency).encode("utf-8")).hexdigest()
     json_dict["q9"] = "How many directories have no immediate file children?"
-    json_dict["a9"] = hl.sha256(str(question_nine_num_dirs_no_immediate_file_children)).hexdigest()
+    json_dict["a9"] = hashlib.sha256(str(question_nine_num_dirs_no_immediate_file_children).encode("utf-8")).hexdigest()
     json_dict["q10"] = "What is the frequency of the word appearing the maximum number of times in files, as file names, and as directory names?"
-    json_dict["a10"] = hl.sha256(str(question_ten_word_frequency)).hexdigest()
-    json_str = j.dump(json_dict)
+    json_dict["a10"] = hashlib.sha256(str(question_ten_word_frequency).encode("utf-8")).hexdigest()
+    json_str = j.dumps(json_dict)
     outfile = open(cache_name, "w")
     outfile.write(json_str)
     outfile.close()
     print("Generation finished! The file tree {} has been created for you".format(GAME_NAME))
+
 if __name__ == "__main__":
     parser = ap.ArgumentParser(description="A file tree search game for Linux")
     subparsers = parser.add_subparsers()
@@ -253,15 +286,12 @@ if __name__ == "__main__":
     parser_create.add_argument('-w', type=int, default=500, dest="max_words", help="The maximum number of words allowed in a file")
     parser_create.add_argument('--fc', type=int, default=4, dest="max_file_children", help="The maximum number of files a directory can have")
     parser_create.add_argument('--dc', type=int, default=6, dest="max_dir_children", help="The maximum number of directory children a directory can have")
+    parser_create.set_defaults(func=create)
     parser_delete = subparsers.add_parser("delete", help="Delete a linux game instance")
     parser_delete.add_argument("--name", type=str, default="linux_game", dest="delete_name", help="Name of the instance to delete")
+    parser_delete.set_defaults(func=delete)
     parser_check = subparsers.add_parser("check", help="Check your answers to the linux game questions")
     parser_check.add_argument("--name", type=str, default="linux_game", dest="check_name", help="Name of the game instance to check")
-
+    parser_check.set_defaults(func=check)
     args = parser.parse_args()
-    if args.create:
-        create(args.create_name, args.max_dir_children, args.max_file_children, args.max_files, args.max_dirs, args.max_words)
-    elif args.delete:
-        delete(args.delete_name)
-    elif args.check:
-        check(args.check_name)
+    args.func(args)
