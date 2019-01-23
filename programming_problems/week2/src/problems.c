38c93427b82279d2becfe0fc26c01a2c23ecb0b0
Joey Bahary <jbahary2@illinois.edu>
2019-01-23 02:03:24 -0600

    Almost done with week2 problems

--- programming_problems/week2/src/problems.c
+++ programming_problems/week2/src/problems.c
@@ -3,117 +3,71 @@
 #include <stdlib.h>
 /*
     These are a bunch of functions to practice writing
-    c code. They range from easy to challenging.
+    c code to practice memory and file io concepts. They range from easy to challenging.
 */
-
 
 
 /*
-    Write a function that takes in 2 integer pointers, and swaps them.
-    a and b are passed in, and a should point to b and b should point to a.
-
-    @param a an integer pointer
-    @param b an integer pointer
-    @return void
+ * Create a function that takes in a file name, and sums up all the integers in 
+ * the file and returns the sum. The integers are all newline seperated, so no 
+ * parsing should be necessary
+ * @param char * filename
+ * @return int sum: the sume of all the integers in the file.
+ * Note: No integer will exceed the maximum 32-bit integer bounds
+ *
 */
-void swap (int *a, int *b) {
-
-}
-
-/*
-    Write a function that takes in a pointer to an array, and
-    the size of the array, and returns the array's maximum element.
-
-    @param arr the integer array
-    @param size the size of the array
-    @return the maximum element
-*/
-int maximumElement(int *arr, int size) {
+long sumIntegers(char * filename) {
     return 0;
 }
 
 /*
-    Write a function that reverses a c style string.
-
-    @param arr the string array to be reversed
-    @return void
-    ** HINT **
-    Think about swapping the elements to reverse the array.
-    You are only given a pointer, so you can't make a new array.
-
-    You are not given a size of the array here, so you need to find that out first.
-    Think how char arrays are structured. How would you find the size?
+ * Given an array of integers, return the array as two seperate halves.
+ * These can be in any way you like as long as array "A" contains integers
+ * 0 to N/2 - 1 and array "B" contains integers N/2 to N - 1.
+ * Note: There is a very easy way to do this. See if you can find it.
+ * @param int * orig_arr: Original array of integers to split
+ * @param int size: Size of the original array to help you split it
+ * @param int ** A: First half of the array
+ * @param int ** B: Second half of the array
+ * @return void
+ *
 */
-void reverseString(char *str) {
-
+void splitArr(int * orig_arr, int size, int ** A, int ** B) {
 }
 
 /*
-    Write a function that finds the absolute difference between all
-    the odd numbers summed, and all the even numbers summed.
-
-    @param arr the integer array containing no negative numbers
-    @param size the size of the array
-    @return the absolute difference between the even numbers summed
-    and the odd numbers summed
+ * Given an array of words return a new array will all the words removed that 
+ * begin with a given letter. For example, if the array had
+ * ["Child", "Fun", "Stuff"] and you were given "S", then you would return
+ * an array of length two with ["Child", "Fun"]. Say for the purposes of this
+ * exercise that there everything is case insensitive.
+ * @param char ** words
+ * @param int * size
+ * @param char ** new_words
+ * @param char letter
+ * @return void
+ * Note: In this problem you are given 2 double pointers and a single pointer. 
+ * Since you will be creating an array within the function you will need to use
+ * heap memory. After creating the new array, then simply copy a word over to
+ * your new array if it meets the criteria (check first letter). After copying
+ * all the words make sure to set the size pointer to the new size of the array.
 */
-int evenOddDiff(int *arr, int size) {
-    return 0;
+void removeWords(char ** words, int * size, char ** new_words, char letter) {
 }
 
 /*
-    Given an array of struct student pointers with declarations like this:
-    struct Student {
-        int age;
-        char name[30];
-        char schoolYear[20]; // Either "Freshman", "Sophomore", "Junior", "Senior", or "Graduate"
-        int creditHours;
-    };
-    Determine the number of students who are Seniors over the age of 21 and have greater
-    than 100 credit hours.
-
-    @param student struct array
-    @param size size of the array
-    @return number of students that fit the criteria
+ * Given the names of two files that have lexographically sorted words
+ * seperated by newlines return the name of a new file where all the words
+ * of both files have been merged in lexographical order.
+ *
+ * @param char * filename1: First file to merge
+ * @param char * filename2: Second file to merge
+ * @param char * new_filename: Name of the new file to put the output in.
+ * @return void
+ *
 */
-int seniorStudents(struct Student *students, int size) {
-    return 0;
+void mergeFiles(char * filename1, char * filename2, char * newfilename) {
 }
 
-/*
-    Given 2 character arrays determine which one comes lexicographically before the other.
-    Return 1 if str1 is lexicographically before str2, return -1 if str2 comes before str1, and
-    return 0 if they are both exactly equal.
-    For example, aaaa comes before aaab. In this case the function would return 1.
-    The strings must be the same length to return 0
 
-    @param str1 string one
-    @param str2 string two
-    @return 1, 0, or -1
-*/
-int strComp(char *str1, char *str2) {
-    return 0;
-}
 
-/*
-    Given an string return its integer representation.
-    If the string has a negtive sign at the front, then make the integer
-    negative. The string given will always be within integer range.
-    "-5000" gives -5000 in integer form
-    @param string given as a char array
-    @return the integer representation.
-
-    ** HINT ** How could you construct the integer using * and +
-    operators?
-
-/*
-    Given a tic-tac-toe board, determine if anyone has won. If X's have won, then return 'X'.
-    If O's have won, then return 'O'. If more than one player has won, or nobody has won, return 'C'.
-    Blank spaces are represented with a ' ' space character.
-    @param the board given as a 3x3 char array
-    @return the char representing the outcome.
-*/
-
-char hasPlayerWon (char board[3][3]) {
-    return 'C';
-}
